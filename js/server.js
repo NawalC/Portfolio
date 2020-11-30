8b5b891be51648c47bcd6169e7bf31ef3c692744
F Nawal Abdul Careem <nwal.careem@gmail.com>
2020-11-30 01:54:20 +0000

    trying to deploy

---
+++ js/server.js
@@ -0,0 +1,6 @@
+const publicPath = path.join(__dirname, "build", "public");
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
