6b904b5191684376139a8f6858430597db40d3ce
F Nawal Abdul Careem <nwal.careem@gmail.com>
2020-11-30 21:08:00 +0000

    removed html files from public folder

--- js/server.js
+++
@@ -1,6 +0,0 @@
-const publicPath = path.join(__dirname, "build", "public");
-app.use(express.static(publicPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
